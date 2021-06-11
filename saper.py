27c46aeb7b893ce10812066849cb0f9674c6dfa5
NowReborngg <NowReborngg@gmail.com>
2021-06-11 13:09:35 +0300

    Thing

--- saper.py
+++ saper.py
@@ -8,20 +8,50 @@ MINEFIELD = []
 SIZE_X = 10
 SIZE_Y = 10
 MINE_COUNT = 10
+OPEN_COUNT = 0
+MARK_COUNT = 0
 TILE_SIZE = 50
+
+def itemClicked(event):
+    global FIELD,MINEFIELD
+    canvas_item_id = c.find_withtag('current')[-1]
+    for row in FIELD:
+        for tile in row:
+            if tile.tile == canvas_item_id:
+                if not tile.open:
+                    tile.open_cell()
+                return
+
+def itemClickedRight(event):
+    global FIELD,MINEFIELD
+    canvas_item_id = c.find_withtag('current')[-1]
+    for row in FIELD:
+        for tile in row:
+            if tile.tile == canvas_item_id:
+                if not tile.marked:
+                    tile.mark_cell()
+                return
+
+
+def GameOver():
+    c.pack_forget()
+    l=Label(root,text="U lose")
+    l.pack()
 
 class Tile():
     def __init__(self,row,cell):
         global TILE_SIZE
-        self.tile = c.create_rectangle(row*TILE_SIZE,
-        cell*TILE_SIZE,
-        (row+1)*TILE_SIZE,
+        self.tile = c.create_rectangle(cell*TILE_SIZE,
+        row*TILE_SIZE,
         (cell+1)*TILE_SIZE,
+        (row+1)*TILE_SIZE,
         fill = 'gray',
         outline="black")
-        self.text = c.create_text((row+0.5)*TILE_SIZE,(cell+0.5)*TILE_SIZE,text="")
+        self.text = c.create_text((cell+0.5)*TILE_SIZE,(row+0.5)*TILE_SIZE,text="")
         self.row = row
         self.cell = cell
+        self.open = False
+        self.marked = False
         return None
 
     def open_cell(self):
@@ -29,14 +59,33 @@ class Tile():
         content = MINEFIELD[self.row][self.cell]
         if content =='m':
             c.itemconfig(self.tile,fill='red')
+            GameOver()
         elif content:
             c.itemconfig(self.tile,fill='green')
+            c.itemconfig(self.text,text=str(content))
         else:
             c.itemconfig(self.tile,fill='white')
         root.update()
+    
+    def mark_cell(self):
+        global MINEFIELD
+        content = MINEFIELD[self.row][self.cell]
+        if content =='m':
+            c.itemconfig(self.tile,fill='red')
+            GameOver()
+        elif content:
+            c.itemconfig(self.tile,fill='green')
+            c.itemconfig(self.text,text=str(content))
+        else:
+            c.itemconfig(self.tile,fill='white')
+        root.update()        
+
+
 
 root = Tk()
 c = Canvas(root,width = TILE_SIZE*SIZE_X,height = TILE_SIZE*SIZE_Y,bg="white")
+c.bind("<Button-1>",itemClicked)
+c.bind("<Button-3>",itemClicked)
 c.pack()
 
 
@@ -49,8 +98,5 @@ for row in range(SIZE_X):
             MINEFIELD[row][cell] = len(matrixFunctions.find_neighbours_equal(MINEFIELD,cell,row,'m'))
 
 FIELD = [[Tile(row,cell) for cell in range(SIZE_X)] for row in range(SIZE_Y)]
-print(*MINEFIELD,sep = '\n')
-for row in range(SIZE_X):
-    for cell in range(SIZE_Y):
-        FIELD[row][cell].open_cell()
+
 root.mainloop()
