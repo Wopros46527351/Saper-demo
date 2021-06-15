fa6989beaa6acaed4cda81116ce93aaaba65c186
NowReborngg <NowReborngg@gmail.com>
2021-06-15 12:25:20 +0300

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -13,6 +13,19 @@ class Minesweeper():
             self.is_marked = False
             self.tile = self.parent_canvas.create_rectangle(cell*tile_size+2,row*tile_size+2,(cell+1)*tile_size+2,(row+1)*tile_size+2,
             fill = "gray",outline = "black")
+            self.text = self.parent_canvas.create_text((cell+0.5)*tile_size+2,(row+0.5)*tile_size+2,text="")
+            self.content = 0
+            self.mates = []
+        
+        def open(self):
+            if self.is_mine:
+                self.parent_canvas.itemconfig(self.tile,fill="red")
+            elif self.content:
+                self.parent_canvas.itemconfig(self.tile,fill="green")
+                self.parent_canvas.itemconfig(self.text,text = self.content)
+            else:
+                self.parent_canvas.itemconfig(self.tile,fill="white")
+            self.is_open=True
 
     def __init__(self,size_y,size_x,mine_dencity) -> None:
         self.TILE_SIZE = 20
@@ -22,8 +35,16 @@ class Minesweeper():
         self.SIZE_X = size_x
         self.SIZE_Y = size_y
         self.MINE_COUNT = int(size_y*size_x*(mine_dencity/100))
-        self.tiles = self.Tile(0,0,True,self.canvas,self.TILE_SIZE)
-    
+
+        #<------------------------------------------------------------------------->
+        #rewrite to use decorators
+        mines = random.sample(range(size_x*size_y),self.MINE_COUNT)
+        self.tiles = [[self.Tile(row,cell,row*size_x+cell in mines,self.canvas,self.TILE_SIZE) for cell in range(size_x)] for row in range(size_y)]
+        for row in range(size_y):
+            for cell in range(size_x):
+                self.tiles[row][cell].mates = find_neighbours(self.tiles,cell,row)
+                self.tiles[row][cell].content = len([0 for m_row,m_cell in self.tiles[row][cell].mates if self.tiles[m_row][m_cell].is_mine])
+
     
 
 
@@ -32,4 +53,8 @@ class Minesweeper():
 
 if __name__=="__main__":
     m = Minesweeper(10,10,10)
+    for row,x in enumerate(m.tiles):
+        for cell,_ in enumerate(x):
+            m.tiles[row][cell].open()
+            m.window.update()
     m.window.mainloop()
