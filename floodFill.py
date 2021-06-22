db6faf873b4a8a66bad59e9a118dfa0dd4cf38d2
NowReborngg <NowReborngg@gmail.com>
2021-06-22 12:16:35 +0300

    Update floodFill.py

--- floodFill.py
+++ floodFill.py
@@ -4,7 +4,7 @@ test_matrix = [
     [0,0,0,1,0,0,0],
     [0,0,0,1,0,0,0],
     [0,0,0,1,0,0,0],
-    [1,1,1,1,1,1,1],
+    [1,1,0,1,0,1,1],
     [0,0,0,1,0,0,0],
     [0,0,0,1,0,0,0],
     [0,0,0,1,0,0,0]]
@@ -21,7 +21,7 @@ def flood_fill_rec(matrix,y,x,match,fill):
     if matrix[y][x] == match:
         matrix[y][x] = fill
         for dy,dx in ((1,0),(-1,0),(0,1),(0,-1)):
-            if 0<=x+dx<=limit_x and 0<=y+dy<=limit_y:
+            if 0<=x+dx<limit_x and 0<=y+dy<limit_y:
                 flood_fill_rec(matrix,y+dy,x+dx,match,fill)
         return matrix
     else:
@@ -32,15 +32,19 @@ def flood_fill_stack(matrix,y,x,match,fill):
     limit_x = len(matrix[0])
     stack = [(y,x)]
     while stack:
+        
         ty,tx = stack.pop()
         if matrix[ty][tx] == match:
             matrix[ty][tx] = fill
             for dy,dx in ((1,0),(-1,0),(0,1),(0,-1)):
-                if 0<=tx+dx<=limit_x and 0<=ty+dy<=limit_y:
-                    stack.append((ty,tx))
+                if 0<=tx+dx<limit_x and 0<=ty+dy<limit_y:
+                    stack.append((ty+dy,tx+dx))
     return matrix
 
 
+def flood_fill_span(matrix,y,x,match,fill):
+    seeds = [(y,x)]
+    while
 
 
 show(test_matrix)
