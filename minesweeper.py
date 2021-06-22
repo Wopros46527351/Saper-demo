bdb431cf3962a39664dd79f9c6f7409e5d73a8c0
Wopros46527351 <77889318+Wopros46527351@users.noreply.github.com>
2021-06-22 14:43:42 +0300

    122333456
    54

--- minesweeper.py
+++ minesweeper.py
@@ -1,5 +1,6 @@
 from tkinter import Tk,Canvas
 from matrixFunctions import *
+import time
 
 
 class Tile():
@@ -38,6 +39,23 @@ class Tile():
                 self.parent_canvas.itemconfig(self.tile,fill="blue")
                 return 1 if self.is_mine else 0
 
+    def fill(self,color):
+        self.parent_canvas.itemconfig(self.tile,fill=color)
+        
+
+    def restore_color(self):
+        if self.is_open:
+            if self.is_mine:
+                self.parent_canvas.itemconfig(self.tile,fill="red")
+            elif self.is_marked:
+                self.parent_canvas.itemconfig(self.tile,fill="blue")
+            elif self.content:
+                self.parent_canvas.itemconfig(self.tile,fill="green")
+            else:
+                self.parent_canvas.itemconfig(self.tile,fill="white")
+        else: 
+            self.parent_canvas.itemconfig(self.tile,fill="gray")
+
 class Minesweeper():
 #<------------------------------------------------------------------------------------------------------------------------------------------------------------------->
 #Canvas click functions
@@ -46,7 +64,10 @@ class Minesweeper():
         for row in self.tiles:
             for cell in row:
                 if cell.tile == item or cell.text ==item:
-                    cell.open()
+                    if cell.content==0:
+                        self.flood_fill_stack(cell)
+                    else:
+                        cell.open()
 
     def mark_cell(self,event):
         item = self.canvas.find_withtag('current')[0]
@@ -61,7 +82,7 @@ class Minesweeper():
     def __init__(self,size_y,size_x,mine_dencity) -> None:
         self.TILE_SIZE = 20
         self.window = Tk()
-        self.canvas = Canvas(self.window,width=self.TILE_SIZE*size_y +2,height = self.TILE_SIZE*size_x+2)
+        self.canvas = Canvas(self.window,width=self.TILE_SIZE*size_x+2,height = self.TILE_SIZE*size_y+2)
         self.canvas.bind("<Button-1>",self.open_cell)
         self.canvas.bind("<Button-3>",self.mark_cell)
         self.canvas.pack()
@@ -78,7 +99,35 @@ class Minesweeper():
                 self.tiles[row][cell].mates = find_neighbours(self.tiles,cell,row)
                 self.tiles[row][cell].content = len([0 for m_row,m_cell in self.tiles[row][cell].mates if self.tiles[m_row][m_cell].is_mine])
 
-    
+#<------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+#rewrite to use decorators
+    def flood_fill_stack(self,tile):
+        if not tile.is_open and tile.content==0:
+            stack=[tile]
+            while stack:
+                target= stack.pop()
+                target.fill('yellow')
+                self.window.update()
+                time.sleep(0.05)
+                target.open()
+                
+                for y,x in target.mates:
+                    mate=self.tiles[y][x]
+                    
+
+                    if not mate.is_open and mate.content==0:
+                        stack.append(mate)
+                    if not mate.is_open:
+                        mate.fill('violet')
+                        self.window.update()
+                        time.sleep(0.05)
+                        mate.open()
+                        mate.restore_color()
+
+
+
+
+                target.restore_color()
     
 
 
@@ -86,7 +135,7 @@ class Minesweeper():
 
 
 if __name__=="__main__":
-    m = Minesweeper(10,10,10)
+    m = Minesweeper(35,70,5)
     """
     for row,x in enumerate(m.tiles):
         for cell,_ in enumerate(x):
