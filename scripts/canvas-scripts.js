b5541a9d609fe236042dd318f6295ffa53551918
Wopros46527351 <77889318+Wopros46527351@users.noreply.github.com>
2021-06-29 15:30:30 +0300

    1234567890
    576tygh

--- scripts/canvas-scripts.js
+++ scripts/canvas-scripts.js
@@ -1,13 +1,85 @@
 function draw() {
+    setInterval(Timer, 1000)
     var canvas = document.getElementById('tutorial');
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
-
+        /*
         ctx.fillStyle = 'rgb(200, 0, 0)';
         ctx.fillRect(10, 10, 50, 50);
 
-        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        ctx.fillStyle = 'rgba(0, 0, 200, 1)';
         ctx.fillRect(30, 30, 50, 50);
+
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+        ctx.fillRect(0, 0, 500, 500);
+        
+        for (var x = 0; x < 500; x += 10) {
+            for (var y = 0; y < 500; y += 10) {
+                ctx.fillStyle = 'rgb(0,0,0)';
+                ctx.fillRect(x, y, 10, 10);
+                ctx.fillStyle = 'rgb(200,200,200)';
+                ctx.fillRect(x + 1, y + 1, 8, 8);
+            }
+        }
+        
+        ctx.fillStyle = 'rgb(255,0,0)';
+        ctx.beginPath();
+        ctx.moveTo(50, 50);
+        ctx.lineTo(100, 100);
+        ctx.lineTo(50, 150);
+        ctx.lineTo(0, 100);
+        ctx.fill();
+        ctx.closePath();
+        */
+        Hex(100, 100, 50, 'rgb(200,200,200)', ctx);
+        Circle(100, 100, 50, ctx)
+
     }
-    alert("hi");
 }
+function Hex(x, y, len, color, ctx) {
+    var pi = Math.PI;
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    for (var i = 90; i <= 450; i += 60) {
+        var x1 = x + Math.cos(i * (pi / 180)) * len;
+        var y1 = y + Math.sin(i * (pi / 180)) * len;
+        ctx.lineTo(x1, y1);
+
+    }
+    ctx.fill();
+    ctx.closePath();
+
+}
+function Circle(x, y, len, ctx) {
+    ctx.beginPath();
+    ctx.arc(x, y, len, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.closePath();
+
+
+
+}
+function Timer() {
+    let elem = document.getElementById('timer')
+    var str = elem.innerText;
+    var str = str.split(':');
+    var min = parseInt(str[0]);
+    var sec = parseInt(str[1]);
+    sec += 1
+    if (sec == 60) {
+        min += 1;
+        sec = 0;
+    }
+    if (min < 10) {
+        min = '0' + min.toString();
+    }
+    if (sec<10){
+        sec='0'+sec.toString();
+    }
+    var new_str = min + ':' + sec;
+    elem.innerText = new_str;
+}
+
+
+
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,7 @@
 let good = false;
 let fast = false;
 let cheap = false;
+let timer_exist = false;
 const easy = 8
 const medium = 12
 const hard = 15
@@ -10,22 +11,25 @@ const hard = 15
 
 
 function hello() {
+    
+    start_timer();
+    
     let elem = document.getElementById("minefield");
-    if (elem !=null){
+    if (elem != null) {
         elem.remove();
     }
-    
+
     let select = document.getElementById("diff-select");
     let diff = select.value;
     let size_x;
     let size_y;
-    if (diff == "easy"){
+    if (diff == "easy") {
         size_x = easy;
         size_y = easy;
-    }else if (diff == "med"){
+    } else if (diff == "med") {
         size_x = medium;
         size_y = medium;
-    }else{
+    } else {
         size_x = hard;
         size_y = hard;
     }
@@ -41,8 +45,8 @@ function hello() {
             let cell = document.createElement('td');
             cell.classList.add('mine-cell');
             cell.classList.add('mine-cell-inactive');
-            cell.style.height = (600/size_y).toString()+"px";
-            cell.style.width = (600/size_x).toString()+"px";
+            cell.style.height = (600 / size_y).toString() + "px";
+            cell.style.width = (600 / size_x).toString() + "px";
             cell.id = x.toString() + "-" + y.toString()
             let content = document.createTextNode(x.toString() + "-" + y.toString());
             cell.appendChild(content);
@@ -52,30 +56,62 @@ function hello() {
 }
 
 
-function s_cheap(){
+function s_cheap() {
     let elem = document.getElementById('cheap')
     cheap = elem.checked;
-    if (cheap && good && fast){
+    if (cheap && good && fast) {
         fast = false;
         document.getElementById('fast').checked = false;
     }
 }
 
-function s_fast(){
+function s_fast() {
     let elem = document.getElementById('fast')
     fast = elem.checked;
-    if (cheap && good && fast){
+    if (cheap && good && fast) {
         good = false;
         document.getElementById('good').checked = false;
     }
 }
 
-function s_good(){
+function s_good() {
     let elem = document.getElementById('good')
     good = elem.checked;
-    if (cheap && good && fast){
+    if (cheap && good && fast) {
         cheap = false;
         document.getElementById('cheap').checked = false;
     }
+}
+
+function Timer() {
+    let elem = document.getElementById('timer')
+    var str = elem.innerText;
+    var str = str.split(':');
+    var min = parseInt(str[0]);
+    var sec = parseInt(str[1]);
+    sec += 1
+    if (sec == 60) {
+        min += 1;
+        sec = 0;
+    }
+    if (min < 10) {
+        min = '0' + min.toString();
+    }
+    if (sec < 10) {
+        sec = '0' + sec.toString();
+    }
+    var new_str = min + ':' + sec;
+    elem.innerText = new_str;
+}
+function start_timer(){
+    let elem = document.getElementById('timer');
+    elem.innerText='00:00';
+    if(!timer_exist){
+        setInterval(Timer, 1000);
+        timer_exist=true;
+
+
+    }
+    
 }
 
